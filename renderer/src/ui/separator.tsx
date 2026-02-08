8568d39410f62dac82d9b14ee3f9fa051db76ef9
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2026-02-08 11:44:09 -0500

    Migrate separator.tsx to shadcn v4 cn-* classes

--- renderer/src/ui/separator.tsx
+++ renderer/src/ui/separator.tsx
@@ -15,7 +15,8 @@ function Separator({
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-px data-[orientation=vertical]:self-stretch",
+        "cn-separator",
+        orientation === "horizontal" ? "cn-separator-horizontal" : "cn-separator-vertical",
         className
       )}
       {...props}
