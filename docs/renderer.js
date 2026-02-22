e7e74903824c6c8099a8369e0ee3a6df5c09d213
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2026-02-22 08:53:08 -0500

    Rebuild docs/renderer.js with v0.3.0 CDN URL (#129)

--- docs/renderer.js
+++ docs/renderer.js
@@ -1,1 +1,1 @@
-(function(){var base=window.location.hostname==="localhost"?"/":"https://cdn.jsdelivr.net/npm/@prefecthq/prefab-ui@0.0.0/dist/";window.__prefabReady=import(base+"_chunks/embed-DVX88IE0.mjs");})();
+(function(){var base=window.location.hostname==="localhost"?"/":"https://cdn.jsdelivr.net/npm/@prefecthq/prefab-ui@0.3.0/dist/";window.__prefabReady=import(base+"_chunks/embed-DNN8TWNC.mjs");})();
